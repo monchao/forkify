e58dfd9c6081924f3a09f3f5d873ca88ebd02b0d
mengzhao <280810593@qq.com>
2021-02-26 13:19:45 -0500

    Add guard for search results rendering.

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -5,8 +5,12 @@ export default class View {
   _data;
 
   render(data) {
+    if (!data || (Array.isArray(data) && data.length === 0)) {
+      return this.renderError();
+    }
+
     this._data = data;
-    const markup = this._generateMarkup(); // ❓
+    const markup = this._generateMarkup();
     this._clear();
     this._parentElement.insertAdjacentHTML("beforeend", markup);
   }
