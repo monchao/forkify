9acd11f3066e06047a8a4d4d8f1822c7a791aee2
mengzhao <280810593@qq.com>
2021-02-28 23:15:54 -0500

    Extract PreviewView.

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,34 +1,17 @@
 import icons from "url:../../img/icons.svg";
+import previewView from "./previewView.js";
 
 import View from "./View.js";
 
 class ResultsView extends View {
   _parentElement = document.querySelector(".results");
-  _errorMessage = "No recipes found for your query! Please try again.";
+  _errorMessage = "No recipe found for your query! Please try again ;)";
   _mesage = "";
 
   _generateMarkup() {
-    return this._data.map(this._generatePreview).join("");
-  }
-
-  _generatePreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-      <li class="preview">
-        <a class="preview__link ${
-          result.id === id ? "preview__link--active" : ""
-        }" href="#${result.id}">
-          <figure class="preview__fig">
-            <img src="${result.imgUrl}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-         </div>
-        </a>
-      </li>
-    `;
+    return this._data
+      .map((result) => previewView.render(result, false))
+      .join("");
   }
 }
 
