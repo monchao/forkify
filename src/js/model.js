b2f2ac066e4f6ea352e0019912d258645f9d9b93
mengzhao <280810593@qq.com>
2021-03-01 23:07:19 -0500

    Store bookmarks in localStorage.

--- src/js/model.js
+++ src/js/model.js
@@ -80,23 +80,42 @@ export const updateServings = function (newServings) {
   state.recipe.servings = newServings;
 };
 
+const persitBookmarks = function () {
+  localStorage.setItem("bookmarks", JSON.stringify(state.bookmarks));
+};
+
 export const addBookmark = function (recipe) {
   // Add bookmark
   state.bookmarks.push(recipe);
 
   // Mark current recipe as bookmark
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  persitBookmarks();
 };
 
 export const deleteBookmark = function (id) {
   // Delete bookmark
   const index = state.bookmarks.findIndex((el) => el.id === id);
-  console.log("index:", index);
   state.bookmarks.splice(index, 1);
 
   // Mark current recipe as NOT bookmarked
   if (id === state.recipe.id) {
-    console.log("id === state.recipe.id");
     state.recipe.bookmarked = false;
   }
+
+  persitBookmarks();
 };
+
+const init = function () {
+  const storage = localStorage.getItem("bookmarks");
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear("bookmarks");
+};
+
+// clearBookmarks()
