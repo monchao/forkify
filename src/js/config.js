e4ac6dcfc7351b5e74a519b52b0932216a192048
mengzhao <280810593@qq.com>
2021-03-02 14:22:30 -0500

    Add API KEY.

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = "3ba0ab23-dabc-4006-ac78-b4ddb85f1251";
