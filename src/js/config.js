3d7d915bacf3e972f30829af8ca82bd32c0bf9bf
mengzhao <280810593@qq.com>
2021-03-05 21:58:21 -0500

    Add MODAL_CLOSE_SEC.

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = "3ba0ab23-dabc-4006-ac78-b4ddb85f1251";
+export const MODAL_CLOSE_SEC = 2.5;
