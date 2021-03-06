d8075596b9dd2c78263d53c045deee1aaa2c0c2a
mengzhao <280810593@qq.com>
2021-03-06 11:35:21 -0500

    Add FORM_REST_SEC.

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = "3ba0ab23-dabc-4006-ac78-b4ddb85f1251";
 export const MODAL_CLOSE_SEC = 2.5;
+export const FORM_REST_SEC = 2.5;
