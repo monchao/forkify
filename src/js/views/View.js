176c6f2fdb4b9e880b9468ab5302437b06da50f3
mengzhao <280810593@qq.com>
2021-03-15 14:51:39 -0400

    Add doc for render.

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,6 +4,16 @@ import icons from "url:../../img/icons.svg"; // Parcel 2
 export default class View {
   _data;
 
+  /**
+   * render
+   * Render the received object to the DOM
+   * @param {Object|Object[]} data The data to be rendered
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM.
+   * @returns {undefined|string} A markup string is returned if render=false.
+   * @this {Object} View instance
+   * @author Moni
+   * @todo Finishing implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0)) {
       return this.renderError();
